aab37a30dd8bc04f0487191881a15972e4033a77
thugmek <tomekmichalhu@seznam.cz>
2024-01-14 11:50:44 +0100

    404 Page image added

--- Frontend/react-bootstrap/src/pages/NotFound.jsx
+++ Frontend/react-bootstrap/src/pages/NotFound.jsx
@@ -10,11 +10,14 @@ function NotFound() {
         <title>404 - Not Found</title>
       </Helmet>
       <main className="container-fluid">
-        <div className="px-4 py-5 my-5 text-center">
+        <div className="px-4 py-4 text-center">
           <h1 className="display-5 fw-bold">404</h1>
           <div className="col-lg-6 mx-auto">
             <p className="lead mb-4">
-              Resource Not Found at {location.pathname}.
+              Resource Not Found.
+            </p>
+            <p>
+            <img className='box-image-smaller' src="/NotFound.png" />
             </p>
             <Link to="/">Navigate back to Home</Link>
           </div>
