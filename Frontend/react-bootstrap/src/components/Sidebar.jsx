99a8fcbbbc3d5aac2b2d5537b276f2092b6bebff
thugmek <tomekmichalhu@seznam.cz>
2024-01-14 11:51:34 +0100

    Menu horizontal separator

--- Frontend/react-bootstrap/src/components/Sidebar.jsx
+++ Frontend/react-bootstrap/src/components/Sidebar.jsx
@@ -7,6 +7,8 @@ import './sidebar.css';
 function Sidebar() {
   const items = [
     { path: '/', title: 'Search', icon: <FiSearch/> },
+    { path: '/labels-generator', title: 'Labels Generator', icon: <FiPenTool/> },
+    { separator: true },
     { path: '/base_image', title: 'Base Image', icon: <FiImage/> },
     { path: '/labels-generator', title: 'Labels Generator', icon: <FiPenTool/> },
   ];
@@ -17,6 +19,9 @@ function Sidebar() {
         <ul className="nav flex-column">
           {
             items.map((item, i) => (
+              (item.separator)?
+              <hr/>
+              :
               <li key={i} className="nav-item">
                 <NavLink className="nav-link" end to={item.path}>
                   <h5>{item.icon} {item.title}</h5>
