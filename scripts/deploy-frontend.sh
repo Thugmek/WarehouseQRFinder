b4ecf8e5407df989dd52c5323f961c7f3418b30d
thugmek <tomekmichalhu@seznam.cz>
2024-01-21 17:03:49 +0100

    npm install on deploy script

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -3,6 +3,7 @@
 export NODE_OPTIONS=--openssl-legacy-provider
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+npm install --prefix "$parent_path/../Frontend/react-bootstrap"
 npm run --prefix "$parent_path/../Frontend/react-bootstrap" build
 
 rm -rf /var/www/
