6d7d63c2856603bd33b62aa7e77003d53cfc7f83
Michal Tomek <tomekmichalhu@seznam.cz>
2024-02-02 10:59:47 +0100

    Api path fix

--- Frontend/react-bootstrap/src/common/constants.js
+++ Frontend/react-bootstrap/src/common/constants.js
@@ -1,7 +1,7 @@
 // nginx
-//const backend_server = "api"
+const backend_server = "api"
 // production
-const backend_server = "https://10.1.20.116/api"
+//const backend_server = "https://10.1.20.116/api"
 
 export {
   backend_server,
