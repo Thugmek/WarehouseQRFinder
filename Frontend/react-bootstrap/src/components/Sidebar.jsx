7811555c591a4eafd29984e6554a258736231845
Michal Tomek <tomekmichalhu@seznam.cz>
2024-10-08 15:43:46 +0200

    Hide config by default

--- Frontend/react-bootstrap/src/components/Sidebar.jsx
+++ Frontend/react-bootstrap/src/components/Sidebar.jsx
@@ -1,36 +1,50 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { BiSearchAlt, BiLayer } from 'react-icons/bi';
 import { FiSettings, FiSearch, FiLayers, FiUser, FiImage, FiPenTool } from 'react-icons/fi';
+import { Form, InputGroup} from 'react-bootstrap';
 
 import './sidebar.css';
 
 function Sidebar() {
   const items = [
-    { path: '/', title: 'Search', icon: <FiSearch/> },
-    { path: '/labels-generator', title: 'Labels Generator', icon: <FiPenTool/> },
-    { separator: true },
-    { path: '/base_image', title: 'Base Image', icon: <FiImage/> },
-    { path: '/image-sources', title: 'Image Sources', icon: <FiImage/> },
+    { path: '/', title: 'Search', icon: <FiSearch/>, is_admin: false },
+    { path: '/labels-generator', title: 'Labels Generator', icon: <FiPenTool/>, is_admin: false },
+    { separator: true, is_admin: true},
+    { path: '/image-sources', title: 'Image Sources', icon: <FiImage/>, is_admin: true },
     //{ path: '/labels-generator', title: 'Labels Generator', icon: <FiPenTool/> },
   ];
 
+  const [adminView, setAdminView] = useState(false);
+
   return (
     <>
-      <div className="position-sticky pt-3">
+      <div className="position-sticky pt-3" style={{"minHeight": "100%"}}>
         <ul className="nav flex-column">
           {
             items.map((item, i) => (
-              (item.separator)?
-              <hr/>
+              (item.is_admin && !adminView)?"":
+              ((item.separator)?
+              <hr key={i} />
               :
               <li key={i} className="nav-item">
                 <NavLink className="nav-link" end to={item.path}>
                   <h5>{item.icon} {item.title}</h5>
                 </NavLink>
-              </li>
+              </li>)
             ))
           }
         </ul>
+        <div className="position-absolute top-100 start-0">
+        <Form style={{"transform": "translate(20px, -30px)"}}>
+          <Form.Check // prettier-ignore
+            type="switch"
+            value={adminView}
+            label="extra config"
+            onChange={(e) => setAdminView(e.target.checked)}
+          />
+        </Form>
+        </div>
       </div>
     </>
   );
